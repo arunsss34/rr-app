d8c3c980a8496e742f6cc7d28a34145b798a3913
@ashok.techorc <ashok@techorc.in>
2023-08-22 15:47:41 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import Service from './components/service';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/Get A Quote!" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
